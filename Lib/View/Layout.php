6b1810a766e77976c797c522b11b667eb2a0c37b
Jeremy Harris <jharris@rockharbor.org>
2013-01-31 09:01:04 -0800

    Added stylesheet

--- Lib/View/Layout.php
+++ Lib/View/Layout.php
@@ -3,6 +3,7 @@
 	<head>
 		<title><?php echo $title; ?> | OC Easter 2013</title>
 		<meta charset="UTF-8" />
+		<link rel="stylesheet" href="/css/styles.css" />
 	</head>
 	<body>
 		<?php echo $content; ?>
