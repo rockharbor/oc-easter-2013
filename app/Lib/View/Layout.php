0f3d81cf945b7abe11262bde6d57072d93bcc0a9
Jeremy Harris <jharris@rockharbor.org>
2013-02-04 17:28:29 -0800

    Added jQuery to layout

--- app/Lib/View/Layout.php
+++ app/Lib/View/Layout.php
@@ -4,6 +4,7 @@
 		<title><?php echo $title; ?> | OC Easter 2013</title>
 		<meta charset="UTF-8" />
 		<link rel="stylesheet" href="/css/styles.css" />
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 	</head>
 	<body>
 		<?php echo $content; ?>
