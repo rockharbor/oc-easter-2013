7969c948d17cc9b8f3fcbd21c957454ad9c5103c
Jeremy Harris <jharris@rockharbor.org>
2013-02-06 12:58:33 -0800

    Removed form js in favor of submodule

--- app/Lib/View/Layout.php
+++ app/Lib/View/Layout.php
@@ -7,7 +7,7 @@
 		<link rel="stylesheet" href="/css/styles.css" />
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js"></script>
-		<script src="/js/jquery.form.js"></script>
+		<script src="/js/form/jquery.form.js"></script>
 		<script>
 			$(document).on('click', 'form[data-ajax]', function() {
 				var el = $(this);
