9de9a7d07cb71d51bcab823d97000ace3f622d43
Jeremy Harris <jharris@rockharbor.org>
2013-02-05 16:19:39 -0800

    Added reset css

--- app/Lib/View/Layout.php
+++ app/Lib/View/Layout.php
@@ -3,6 +3,7 @@
 	<head>
 		<title><?php echo $title; ?> | OC Easter 2013</title>
 		<meta charset="UTF-8" />
+		<link rel="stylesheet" href="/css/reset.css" />
 		<link rel="stylesheet" href="/css/styles.css" />
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js"></script>
