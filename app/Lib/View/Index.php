f0076690e2b085f591f260a4bc6cf6f75e97032f
Jeremy Harris <jharris@rockharbor.org>
2013-02-19 11:40:07 -0800

    Added #hash navigation for slideshow

--- app/Lib/View/Index.php
+++ app/Lib/View/Index.php
@@ -1,20 +1,29 @@
 <script>
-	(function($) {
+	$(document).ready(function() {
 		// let the js do the scrolling
 		$('.scroll').css('overflow', 'hidden');
 		// hide content
 		$('.scroll section article').hide();
-
-		// initialize first slide
-		$('.scroll section:first-child article').delay(showDelay).fadeIn();
 
 		var selected = 0;
 		var total = $('.scroll section').length;
 		var w = $('.scroll section').width();
 		var showDelay = 2000;
 
+		// check if this was linked to and take them to the slide
+		var hash = window.location.hash;
+		if (window.history.pushState && $(hash).length > 0) {
+			selected = $('.scroll section').index($(hash));
+			console.log('based on hash '+selected);
+		}
+
+		// initialize slide, and update history if this isn't the first
+		scrollTo(w * selected);
+		showCurrent(selected === 0);
+
 		$('.content').swipe({
 			triggerOnTouchEnd : true,
+			allowPageScroll: false,
 			swipeStatus: function (event, phase, direction, distance) {
 				w = $('.scroll section').width();
 
@@ -34,9 +43,6 @@
 							prev();
 						}
 						break;
-					default:
-						scrollTo(w * selected);
-						showCurrent();
 				}
 			}
 		});
@@ -53,8 +59,15 @@
 			showCurrent();
 		}
 
-		function showCurrent() {
+		function showCurrent(updateHistory) {
+			if (typeof updateHistory === 'undefined') {
+				updateHistory = true;
+			}
 			$('.scroll section:nth-child('+(selected+1)+') article').delay(showDelay).fadeIn();
+			if (updateHistory && window.history.pushState) {
+				var selectedId = $('.scroll section:nth-child('+(selected+1)+')').prop('id');
+				window.history.pushState(null, null, '#'+selectedId);
+			}
 		}
 
 		function scrollTo(distance, duration) {
@@ -83,5 +96,20 @@
 				'transform': tstring
 			});
 		}
-	})(jQuery);
+
+		if (window.history.pushState) {
+			window.addEventListener('popstate', function(e) {
+				selected = $('.scroll section').index($(window.location.hash));
+				scrollTo(w * selected);
+				showCurrent(false);
+			});
+		}
+
+		$('.scroll').scroll(function(e) {
+			// seriously #hash, we don't want you to scroll
+			e.preventDefault();
+			this.scrollLeft = 0;
+			return false;
+		});
+	});
 </script>
