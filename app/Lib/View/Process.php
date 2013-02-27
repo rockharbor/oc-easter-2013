a7774f0406cc754c9f6957c59561959cb44c0fd7
Jeremy Harris <jharris@rockharbor.org>
2013-02-26 16:06:43 -0800

    Tweaked mobile sections, upload section

--- app/Lib/View/Process.php
+++ app/Lib/View/Process.php
@@ -3,14 +3,15 @@ if (isset($error)) {
 	echo '<p class="error">'.$error.'</p>';
 }
 ?>
-<div style="background: url(/image/<?php echo $tmpname; ?>) no-repeat; width: 300px; height: 300px">
+<div class="user-image" style="background: url('/image/<?php echo $tmpname; ?>') no-repeat;">
 	<img class="draggable" src="/img/mark.png" title="Drag Me!" />
 </div>
 <form method="post" action="/victory/process/<?php echo $tmpname; ?>" enctype="multipart/form-data">
 	<input type="hidden" name="x" value="0" />
 	<input type="hidden" name="y" value="0" />
-	<textarea name="note" rows="4" maxlength="140" placeholder="Share your victory"></textarea>
-	<input type="submit" name="submit" value="Declare Victory!" />
+	<textarea name="note" rows="4" maxlength="140" style="width: 300px;" placeholder="Share your victory"></textarea>
+	<br />
+	<input class="button" type="submit" name="submit" value="Declare Victory!" />
 </form>
 <script>
 	$('.draggable').draggable({
--- app/Lib/View/Victory.All.php
+++ app/Lib/View/Victory.All.php
@@ -1,16 +1,18 @@
 <section>
-	<article class="clearfix">
+	<article>
 		<p>
 			<a class="upload-slide-up button" href="/victory/upload">Share Your Victory</a>
 		</p>
-		<?php foreach ($results as $result): ?>
-		<div class="image-container">
-			<img src="/uploads/<?php echo $result->filename; ?>" />
-			<?php if (!empty($result->note)): ?>
-			<p><?php echo $result->note; ?></p>
-			<?php endif; ?>
-		</div>
+		<div class="clearfix">
+			<?php foreach ($results as $result): ?>
+			<div class="image-container">
+				<img src="/uploads/<?php echo $result->filename; ?>" />
+				<?php if (!empty($result->note)): ?>
+				<p><?php echo $result->note; ?></p>
+				<?php endif; ?>
+			</div>
 		<?php endforeach; ?>
+		</div>
 	</article>
 </section>
 <div class="content auto upload-slide">
