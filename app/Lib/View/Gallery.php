5f5084b53a62d2a984525b87812784760449f428
Jeremy Harris <jharris@rockharbor.org>
2013-02-27 13:56:12 -0800

    Added gallery

--- app/Lib/View/Gallery.php
+++ app/Lib/View/Gallery.php
@@ -2,9 +2,92 @@
 	<article>
 		<h1>Gallery</h1>
 		<p>Check out the pictures below for a glimpse of last year&apos;s Easter Celebration. </p>
+		<div class="slideshow clearfix">
+			<img src="/img/gallery/150/1.jpg" />
+			<img src="/img/gallery/150/2.jpg" />
+			<img src="/img/gallery/150/3.jpg" />
+			<img src="/img/gallery/150/4.jpg" />
+			<img src="/img/gallery/150/5.jpg" />
+			<img src="/img/gallery/150/6.jpg" />
+			<img src="/img/gallery/150/7.jpg" />
+			<img src="/img/gallery/150/8.jpg" />
+			<img src="/img/gallery/150/9.jpg" />
+			<img src="/img/gallery/150/10.jpg" />
+			<img src="/img/gallery/150/11.jpg" />
+			<img src="/img/gallery/150/12.jpg" />
+			<img src="/img/gallery/150/13.jpg" />
+			<img src="/img/gallery/150/14.jpg" />
+			<img src="/img/gallery/150/15.jpg" />
+			<img src="/img/gallery/150/16.jpg" />
+			<img src="/img/gallery/150/17.jpg" />
+			<img src="/img/gallery/150/18.jpg" />
+			<img src="/img/gallery/150/19.jpg" />
+			<img src="/img/gallery/150/20.jpg" />
+		</div>
+		<div class="slideshow-large" style="display: none">
+			<div class="slideshow-large-image">
+				<div class="slideshow-popup">
+					<a class="button close" href="#">X</a>
+					<a class="button next" href="#">Next &gt;</a>
+					<a class="button prev" href="#">&lt; Prev</a>
+				</div>
+				<div class="slideshow-background"></div>
+			</div>
+		</div>
 	</article>
 </section>
 
 <div class="background">
 	<div class="bg14" />
 </div>
+
+<script>
+	$('.slideshow-popup a.close, .slideshow-background').click(function() {
+		$('.slideshow-large').fadeOut();
+	});
+	$('.slideshow img').click(function() {
+		var src = $(this).attr('src');
+		var large = src.replace('/150/', '/800/');
+		$('.slideshow-popup img').remove();
+		var img = new Image();
+		img.src = large;
+		img.onload = function() {
+			$('.slideshow-large').fadeIn();
+		}
+		$('.slideshow-popup').append(img).data('showing', src);
+	});
+	$('body').keyup(function(event) {
+		if (!$('.slideshow-large').is(':visible')) {
+			return;
+		}
+		var showing = $('.slideshow-popup').data('showing');
+		switch (event.which) {
+			case 37:
+			case 72:
+				var next = $('.slideshow img[src="'+showing+'"]').prev();
+				if (next.length == 0) {
+					next = $('.slideshow img:last-child');
+				}
+			break;
+			case 39:
+			case 76:
+				var next = $('.slideshow img[src="'+showing+'"]').next();
+				if (next.length == 0) {
+					next = $('.slideshow img:first-child');
+				}
+			break;
+			case 27:
+				$('.slideshow-popup a.close').click();
+			break;
+		}
+		next.click();
+	});
+	$('.next').click(function() {
+		var e = new $.Event('keyup', {which: 39});
+		$('body').trigger(e);
+	});
+	$('.prev').click(function() {
+		var e = new $.Event('keyup', {which: 37});
+		$('body').trigger(e);
+	});
+</script>
