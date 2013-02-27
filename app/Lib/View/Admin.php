e9b9d5b0e62e551422da12cbf07fc997e1416fd5
Jeremy Harris <jharris@rockharbor.org>
2013-02-27 09:57:05 -0800

    Wrapped admin page in section article

--- app/Lib/View/Admin.php
+++ app/Lib/View/Admin.php
@@ -1,43 +1,49 @@
-<table>
-	<thead>
-		<tr>
-			<td>Image</td>
-			<td>Note</td>
-			<td>Submitted</td>
-			<td>Approved</td>
-		</tr>
-	</thead>
-	<tbody>
-		<?php foreach ($results as $result): ?>
+<section>
+	<article>
+		<table>
+			<thead>
+				<tr>
+					<td>Image</td>
+					<td>Note</td>
+					<td>Submitted</td>
+					<td>Approved</td>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach ($results as $result): ?>
+				<?php
+				$form = "<form data-ajax=\"approveComplete\" action=\"/admin/approve/$result->filename\" method=\"post\">";
+				$form .= "<input type=\"submit\" value=\"Approve\" />";
+				$form .= "</form>";
+				?>
+				<tr>
+					<td><img class="small" src="/uploads/<?php echo $result->filename; ?>" /></td>
+					<td><?php echo $result->note; ?></td>
+					<td><?php echo $result->created; ?></td>
+					<td><?php echo $result->approved ? '&#x2713;' : $form; ?></td>
+				</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+		<p>
 		<?php
-		$form = "<form data-ajax=\"approveComplete\" action=\"/admin/approve/$result->filename\" method=\"post\">";
-		$form .= "<input type=\"submit\" value=\"Approve\" />";
-		$form .= "</form>";
+		if ($page-1 > 0) {
+			echo '<a href="/admin/'.($page-1).'">Prev</a>';
+		}
+		$padding = 5;
+		$pages = range(max(1, $page - $padding), min($page + $padding, $maxpages));
+		$c = 0;
+		while ($c < count($pages)) {
+			echo "<a href=\"/admin/$pages[$c]\">$pages[$c]</a>";
+			$c++;
+		}
+		if ($page < $maxpages) {
+			echo '<a href="/admin/'.($page+1).'">Next</a>';
+		}
 		?>
-		<tr>
-			<td><img class="small" src="/uploads/<?php echo $result->filename; ?>" /></td>
-			<td><?php echo $result->note; ?></td>
-			<td><?php echo $result->created; ?></td>
-			<td><?php echo $result->approved ? '&#x2713;' : $form; ?></td>
-		</tr>
-		<?php endforeach; ?>
-	</tbody>
-</table>
-<?php
-if ($page-1 > 0) {
-	echo '<a href="/admin/'.($page-1).'">Prev</a>';
-}
-$padding = 5;
-$pages = range(max(1, $page - $padding), min($page + $padding, $maxpages));
-$c = 0;
-while ($c < count($pages)) {
-	echo "<a href=\"/admin/$pages[$c]\">$pages[$c]</a>";
-	$c++;
-}
-if ($page < $maxpages) {
-	echo '<a href="/admin/'.($page+1).'">Next</a>';
-}
-?>
+		</p>
+	</article>
+</section>
 <script>
 	function approveComplete(data, status, xhr) {
 		if (data == '1') {
