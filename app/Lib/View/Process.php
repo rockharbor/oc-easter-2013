db480f3230711143785d256de5c9a22740903514
Jeremy Harris <jharris@rockharbor.org>
2013-02-26 16:32:36 -0800

    Added touch events for draggable :/

--- app/Lib/View/Process.php
+++ app/Lib/View/Process.php
@@ -13,7 +13,36 @@ if (isset($error)) {
 	<br />
 	<input class="button" type="submit" name="submit" value="Declare Victory!" />
 </form>
+
 <script>
+	function touchHandler(event) {
+		var touches = event.changedTouches,
+		first = touches[0],
+		type = "";
+
+		switch(event.type) {
+		case "touchstart": type = "mousedown"; break;
+		case "touchmove":  type="mousemove"; break;
+		case "touchend":   type="mouseup"; break;
+		default: return;
+		}
+		var simulatedEvent = document.createEvent("MouseEvent");
+		simulatedEvent.initMouseEvent(type, true, true, window, 1,
+			first.screenX, first.screenY,
+			first.clientX, first.clientY, false,
+			false, false, false, 0, null
+		);
+
+		first.target.dispatchEvent(simulatedEvent);
+		event.preventDefault();
+	}
+
+	var draggable = $('.draggable')[0];
+	draggable.addEventListener("touchstart", touchHandler, true);
+	draggable.addEventListener("touchmove", touchHandler, true);
+	draggable.addEventListener("touchend", touchHandler, true);
+	draggable.addEventListener("touchcancel", touchHandler, true);
+
 	$('.draggable').draggable({
 		containment: 'parent',
 		drag: function(event, ui) {
