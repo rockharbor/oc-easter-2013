003c781bac4a779bce2c1e8ba4756f18c5f2fc80
Jeremy Harris <jharris@rockharbor.org>
2013-02-28 08:40:34 -0800

    Restructured church directory

--- app/Lib/View/ChurchDirectory.php
+++ app/Lib/View/ChurchDirectory.php
@@ -1,226 +1,329 @@
 <section>
-	<article>
+	<article style="background: rgb(0,0,0); background: rgba(0,0,0,.8); padding-top: 10px">
 		<h1>Church Directory</h1>
-		<p>Following Jesus is not intended to be done in isolation. After celebrating Easter together, we encourage you to find  a church in your local neighborhood. Worship together. Celebrate together. Walk with Jesus together.</p>
-		<p>Check out this list of great churches in Orange and L.A. counties:</p>
-		<div class="clearfix">
+		<p style="background:transparent">Following Jesus is not intended to be done in isolation. After celebrating Easter together, we encourage you to find  a church in your local neighborhood. Worship together. Celebrate together. Walk with Jesus together.</p>
+		<p style="background:transparent">Check out this list of great churches in Orange and L.A. counties:</p>
+		<div class="directory clearfix">
 			<div class="directory-column">
-				<h2 style="display: block; margin-bottom: 10px;text-align: center;">Orange County</h2>
-				<h2>Brea</h2>
-	<div class="church"><h3>Southlands Church International</h3>
-	<p>714-982-5055<br />
-		<a target="_blank" href="http://www.southlands.net">www.southlands.net</a></p>
-	<p>2950 E. Imperial Hwy., Brea</p>
-	<p>Sundays | 9am &amp; 11am</p>
-	</div>
-	<h2>Costa Mesa</h2>
-	<div class="church"><h3>The Crossing Church</h3>
-	<p>949-645-5050<br />
-		<a target="_blank" href="http://www.thecrossing.com">www.thecrossing.com</a></p>
-	<p>2115 Newport Blvd., Costa Mesa</p>
-	<p>Saturdays | 5:30 pm<br />
-Sundays | 9:15am &amp; 11:15am
-</p>
-	</div>
-	<div class="church"><h3>Newport Mesa Church</h3>
-	<p>714-966-0454<br />
-		<a target="_blank" href="http://www.newportmesa.org">www.newportmesa.org</a></p>
-	<p>2599 Newport Blvd., Costa Mesa</p>
-	<p>Sundays | 9am &amp; 11am</p>
-	</div>
-	<div class="church"><h3>Redemption Church</h3>
-	<p>949-607-8095<br />
-		<a target="_blank" href="http://www.redemptionchurch.org">www.redemptionchurch.org</a></p>
-	<p>Back Bay High School<br />
-		390 Monte Vista Dr., Costa Mesa</p>
-	<p>Sundays | 10am</p>
-	</div>
-	<div class="church"><h3><strong>ROCK</strong>HARBOR Central</h3>
-	<p>714-384-0914<br />
-		<a target="_blank" href="http://www.rockharbor.org">www.rockharbor.org</a></p>
-	<p>345 Fischer Avenue, Costa Mesa</p>
-	<p>Saturdays | 6pm<br />
-Sundays | 9am, 11am, 7pm, 9pm
-</p>
-	</div>
-	<h2>Fullerton</h2>
-	<div class="church"><h3>New Song North OC</h3>
-	<p>714-278-8020<br />
-		<a target="_blank" href="http://noc.newsong.net">noc.newsong.net</a></p>
-	<p>Pacific Auditorium @ Hope International University<br />
-2500 Nutwood Ave., Fullerton
-</p>
-	<p>Sundays | 9:30am  &amp; 11:15am</p>
-	</div>
-	<div class="church"><h3><strong>ROCK</strong>HARBOR Fullerton</h3>
-	<p>714-384-0914<br />
-		<a target="_blank" href="http://fullerton.rockharbor.org">fullerton.rockharbor.org</a></p>
-	<p>102 Bradford Ave<br />
-Placentia
-</p>
-	<p>Sundays | 9am, 11am, 7pm</p>
-	</div>
-	<h2>Huntington Beach</h2>
-	<div class="church"><h3>Mariners Huntington Beach</h3>
-	<p>949-769-8100<br />
-		<a target="_blank" href="http://www.marinerschurch.org">www.marinerschurch.org</a></p>
-	<p>Huntington Beach Central Library Theater<br />
-7111 Talbert Ave., Huntington Beach
-</p>
-	<p>Sundays | 9am &amp; 11am</p>
-	</div>
-	<div class="church"><h3><strong>ROCK</strong>HARBOR Huntington Beach</h3>
-	<p>714-384-0914<br />
-		<a target="_blank" href="http://huntingtonbeach.rockharbor.org">huntingtonbeach.rockharbor.org</a></p>
-	<p>Rodgers Senior Center<br />
-1706 Orange Ave., Huntington Beach
-</p>
-	<p>Sundays | 9am, 11am</p>
-	</div>
-	<h2>Irvine</h2>
-	<div class="church"><h3>Mariners</h3>
-	<p>949-769-8100<br />
-		<a target="_blank" href="http://www.marinerschurch.org">www.marinerschurch.org</a></p>
-	<p>5001 Newport Coast Dr., Irvine</p>
-	<p>Saturdays | 5pm<br />
-Sundays | 9am &amp; 11am in Chapel<br />
-Sundays | 7pm in Community Center</p>
-	</div>
-	<div class="church"><h3>Moment</h3>
-	<p>714-646-6787<br />
-		<a target="_blank" href="http://momentchurch.com">momentchurch.com</a></p>
-	<p>1 Hope Drive, Tustin</p>
-	<p>Sundays | 9am &amp; 11am</p>
-	</div>
-	<div class="church"><h3>New Song Irvine</h3>
-	<p>949-477-0700<br />
-		<a target="_blank" href="http://irvine.newsong.net">irvine.newsong.net</a>
-	</p>
-	<p>18842 Teller Ave., Irvine</p>
-	<p>Sundays | 9am &amp; 10:45am</p>
-	</div>
-	<div class="church"><h3>Pathways Church</h3>
-	<p>714-543-4400<br />
-		<a target="_blank" href="http://ocpathways.org">ocpathways.org</a></p>
-	<p>Hicks Canyon Elementary School<br />
-3817 Viewpark, Irvine</p>
-	<p>Sundays | 10am</p>
-</div>
-	<h2>Mission Viejo</h2>
-	<div class="church"><h3><strong>ROCK</strong>HARBOR Mission Viejo</h3>
-	<p>714-384-0914<br />
-		<a target="_blank" href="http://missionviejo.rockharbor.org">missionviejo.rockharbor.org</a>
-	</p>
-	<p>Los Alisos Intermediate School<br />
-25171 Moor Ave, Mission Viejo
-</p>
-	<p>Sundays | 9am, 11am</p>
-	</div>
-	<div class="church"><h3>Mariners Mission Viejo</h3>
-	<p>949-769-8100<br />
-		<a target="_blank" href="http://www.marinerschurch.org">www.marinerschurch.org</a>
-	</p>
-	<p>26862 Crown Valley Pkwy., Mission Viejo</p>
-	<p>Saturdays | 5pm<br />Sundays | 9am &amp; 11am </p>
-	</div>
-	<h2>Orange</h2>
-	<div class="church"><h3><strong>ROCK</strong>HARBOR Orange</h3>
-	<p>714-384-0914<br />
-		<a target="_blank" href="http://orange.rockharbor.org">orange.rockharbor.org</a>
-	</p>
-	<p>Orange High School<br />
-525 North Shaffer Street, Orange
-</p>
-	<p>Sundays | 10am</p>
-	</div>
-	<div class="church"><h3>Beacon Church</h3>
-	<p>714-633-3351<br />
-		<a target="_blank" href="http://www.thebeaconoc.org">www.thebeaconoc.org</a></p>
-	<p>El Modena High School<br />
-3920 Spring St., Orange
-</p>
-	<p>Sundays | 10:05am</p>
-	</div>
-	<h2>Yorba Linda</h2>
-	<div class="church"><h3>Yorba Linda Friends</h3>
-	<p>714-777-2875<br />
-		<a target="_blank" href="http://www.friendschurchyl.com">www.friendschurchyl.com</a></p>
-	<p>5211 Lakeview Ave., Yorba Linda</p>
-	<p>Saturdays | 6pm <br />
-Sundays | 9am &amp; 10:45am
-</p>
-	</div>
+				<h2>Orange County</h2>
+				<div class="region">
+					<a class="city" href="#brea" id="brea">Brea</a>
+					<div class="churches">
+						<div class="church">
+							<h3>Southlands Church International</h3>
+							<p>
+								714-982-5055<br />
+								<a target="_blank" href="http://www.southlands.net">www.southlands.net</a><br />
+								2950 E. Imperial Hwy., Brea<br />
+								Sundays | 9am &amp; 11am
+							</p>
+						</div>
+					</div>
+					<a class="city" href="#costamesa" id="costamesa">Costa Mesa</a>
+					<div class="churches">
+						<div class="church">
+							<h3>The Crossing Church</h3>
+							<p>
+								949-645-5050<br />
+								<a target="_blank" href="http://www.thecrossing.com">www.thecrossing.com</a><br />
+								2115 Newport Blvd., Costa Mesa<br />
+								Saturdays | 5:30 pm<br />
+								Sundays | 9:15am &amp; 11:15am
+							</p>
+						</div>
+						<div class="church">
+							<h3>Newport Mesa Church</h3>
+							<p>
+								714-966-0454<br />
+								<a target="_blank" href="http://www.newportmesa.org">www.newportmesa.org</a><br />
+								2599 Newport Blvd., Costa Mesa<br />
+								Sundays | 9am &amp; 11am
+							</p>
+						</div>
+						<div class="church">
+							<h3>Redemption Church</h3>
+							<p>
+								949-607-8095<br />
+								<a target="_blank" href="http://www.redemptionchurch.org">www.redemptionchurch.org</a><br />
+								Back Bay High School<br />
+								390 Monte Vista Dr., Costa Mesa<br />
+								Sundays | 10am
+							</p>
+						</div>
+						<div class="church">
+							<h3><strong>ROCK</strong>HARBOR Central</h3>
+							<p>
+								714-384-0914<br />
+								<a target="_blank" href="http://www.rockharbor.org">www.rockharbor.org</a><br />
+								345 Fischer Avenue, Costa Mesa<br />
+								Saturdays | 6pm<br />
+								Sundays | 9am, 11am, 7pm, 9pm
+							</p>
+						</div>
+					</div>
+					<a class="city" href="#fullerton" id="fullerton">Fullerton</a>
+					<div class="churches">
+						<div class="church">
+							<h3>New Song North OC</h3>
+							<p>
+								714-278-8020<br />
+								<a target="_blank" href="http://noc.newsong.net">noc.newsong.net</a><br />
+								Pacific Auditorium @ Hope International University<br />
+								2500 Nutwood Ave., Fullerton<br />
+								Sundays | 9:30am  &amp; 11:15am
+							</p>
+						</div>
+						<div class="church">
+							<h3><strong>ROCK</strong>HARBOR Fullerton</h3>
+							<p>
+								714-384-0914<br />
+								<a target="_blank" href="http://fullerton.rockharbor.org">fullerton.rockharbor.org</a><br />
+								102 Bradford Ave<br />
+								Placentia<br />
+								Sundays | 9am, 11am, 7pm
+							</p>
+						</div>
+					</div>
+					<a class="city" href="#huntingtonbeach" id="huntingtonbeach">Huntington Beach</a>
+					<div class="churches">
+						<div class="church">
+							<h3>Mariners Huntington Beach</h3>
+							<p>
+								949-769-8100<br />
+								<a target="_blank" href="http://www.marinerschurch.org">www.marinerschurch.org</a><br />
+								Huntington Beach Central Library Theater<br />
+								7111 Talbert Ave., Huntington Beach<br />
+								Sundays | 9am &amp; 11am
+							</p>
+						</div>
+						<div class="church">
+							<h3><strong>ROCK</strong>HARBOR Huntington Beach</h3>
+							<p>
+								714-384-0914<br />
+								<a target="_blank" href="http://huntingtonbeach.rockharbor.org">huntingtonbeach.rockharbor.org</a><br />
+								Rodgers Senior Center<br />
+								1706 Orange Ave., Huntington Beach<br />
+								Sundays | 9am, 11am
+							</p>
+						</div>
+					</div>
+					<a class="city" href="#irvine" id="irvine">Irvine</a>
+					<div class="churches">
+						<div class="church">
+							<h3>Mariners</h3>
+							<p>
+								949-769-8100<br />
+								<a target="_blank" href="http://www.marinerschurch.org">www.marinerschurch.org</a><br />
+								5001 Newport Coast Dr., Irvine<br />
+								Saturdays | 5pm<br />
+								Sundays | 9am &amp; 11am in Chapel<br />
+								Sundays | 7pm in Community Center
+							</p>
+						</div>
+						<div class="church">
+							<h3>Moment</h3>
+							<p>
+								714-646-6787<br />
+								<a target="_blank" href="http://momentchurch.com">momentchurch.com</a><br />
+								1 Hope Drive, Tustin<br />
+								Sundays | 9am &amp; 11am
+							</p>
+						</div>
+						<div class="church">
+							<h3>New Song Irvine</h3>
+							<p>
+								949-477-0700<br />
+								<a target="_blank" href="http://irvine.newsong.net">irvine.newsong.net</a><br />
+								18842 Teller Ave., Irvine<br />
+								Sundays | 9am &amp; 10:45am
+							</p>
+						</div>
+						<div class="church">
+							<h3>Pathways Church</h3>
+							<p>
+								714-543-4400<br />
+								<a target="_blank" href="http://ocpathways.org">ocpathways.org</a><br />
+								Hicks Canyon Elementary School<br />
+								3817 Viewpark, Irvine<br />
+								Sundays | 10am
+							</p>
+						</div>
+					</div>
+					<a class="city" href="#missionviejo" id="missionviejo">Mission Viejo</a>
+					<div class="churches">
+						<div class="church">
+							<h3><strong>ROCK</strong>HARBOR Mission Viejo</h3>
+							<p>
+								714-384-0914<br />
+								<a target="_blank" href="http://missionviejo.rockharbor.org">missionviejo.rockharbor.org</a><br />
+								Los Alisos Intermediate School<br />
+								25171 Moor Ave, Mission Viejo<br />
+								Sundays | 9am, 11am
+							</p>
+						</div>
+						<div class="church">
+							<h3>Mariners Mission Viejo</h3>
+							<p>
+								949-769-8100<br />
+								<a target="_blank" href="http://www.marinerschurch.org">www.marinerschurch.org</a><br />
+								26862 Crown Valley Pkwy., Mission Viejo<br />
+								Saturdays | 5pm<br />
+								Sundays | 9am &amp; 11am
+							</p>
+						</div>
+					</div>
+					<a class="city" href="#orange" id="orange">Orange</a>
+					<div class="churches">
+						<div class="church">
+							<h3><strong>ROCK</strong>HARBOR Orange</h3>
+							<p>
+								714-384-0914<br />
+								<a target="_blank" href="http://orange.rockharbor.org">orange.rockharbor.org</a><br />
+								Orange High School<br />
+								525 North Shaffer Street, Orange<br />
+								Sundays | 10am
+							</p>
+						</div>
+						<div class="church">
+							<h3>Beacon Church</h3>
+							<p>
+								714-633-3351<br />
+								<a target="_blank" href="http://www.thebeaconoc.org">www.thebeaconoc.org</a><br />
+								El Modena High School<br />
+								3920 Spring St., Orange<br />
+								Sundays | 10:05am
+							</p>
+						</div>
+					</div>
+					<a class="city" href="#yorbalinda" id="yorbalinda">Yorba Linda</a>
+					<div class="churches">
+						<div class="church">
+							<h3>Yorba Linda Friends</h3>
+							<p>
+								714-777-2875<br />
+								<a target="_blank" href="http://www.friendschurchyl.com">www.friendschurchyl.com</a><br />
+								5211 Lakeview Ave., Yorba Linda<br />
+								Saturdays | 6pm <br />
+								Sundays | 9am &amp; 10:45am
+							</p>
+						</div>
+					</div>
+				</div>
 			</div>
 			<div class="directory-column">
-
-	<h2 style="display: block; margin-bottom: 10px;text-align: center;">Los Angeles County</h2>
-	<h2>Long Beach</h2>
-	<div class="church"><h3>The Garden</h3>
-	<p><a target="_blank" href="http://www.thegardenlb.org">www.thegardenlb.org</a></p>
-	<p>1001 E. 3rd St., Long Beach</p>
-	<p>Sundays | 10am</p>
-	</div>
-	<div class="church"><h3>Grace Brethren</h3>
-	<p>562-595-6881<br />
-		<a target="_blank" href="http://www.gracelb.net">www.gracelb.net</a></p>
-	<p>3601 Linden Ave, Long Beach</p>
-	<p>Sundays | 9:30am &amp; 11am</p>
-	</div>
-	<h2>Los Angeles</h2>
-	<div class="church"><h3>Basileia</h3>
-	<p>323-793-5732<br />
-		<a target="_blank" href="http://www.basileiacommunity.org">www.basileiacommunity.org</a></p>
-	<p>4903 Fountain Ave., Hollywood</p>
-	<p>Sundays | 10:30am</p>
-	</div>
-	<div class="church"><h3>Ecclesia</h3>
-	<p>323-464-8455<br />
-		<a target="_blank" href="http://www.churchinhollywood.com">www.churchinhollywood.com</a></p>
-	<p>The Hollywood Pacific Theatre<br />6433 Hollywood Blvd., Hollywood</p>
-	<p>Sundays | 9:30 &amp; 11:15am</p>
-	</div>
-	<div class="church"><h3>Mosaic Hollywood</h3>
-	<p>626-628-9640<br />
-		<a target="_blank" href="http://www.mosaic.org">www.mosaic.org</a></p>
-	<p>7107 Hollywood Blvd, Hollywood</p>
-	<p>Sundays | 10am, 11:30am, 6pm, 8pm</p>
-	</div>
-	<div class="church"><h3>New Song L.A.</h3>
-	<p>310-842-4215<br />
-		<a target="_blank" href="http://la.newsong.net">la.newsong.net</a></p>
-	<p>11177 Overland Ave., Culver City</p>
-	<p>Sundays | 9:30am &amp; 11:30am</p>
-	</div>
-	<div class="church"><h3>Reality L.A.</h3>
-	<p>323-883-1812<br />
-		<a target="_blank" href="http://www.realityla.com">www.realityla.com</a></p>
-	<p>Helen Bernstein High School<br />1309 N. Wilton Pl., Hollywood</p>
-	<p>Sundays | 9am, 12pm, 5pm</p>
-	</div>
-	<h2>Pasadena</h2>
-	<div class="church"><h3>Fellowship Monrovia</h3>
-	<p>626-239-8109<br />
-		<a target="_blank" href="http://www.madeforfellowship.com">www.madeforfellowship.com</a></p>
-	<p>Monrovia High School<br />845 W. Colorado Blvd., Monrovia</p>
-	<p>Sundays | 9am &amp; 11am</p>
-	</div>
-	<h2>Pomona</h2>
-	<div class="church"><h3>Mosaic Pomona</h3>
-	<p>626-628-9640<br />
-		<a target="_blank" href="http://www.mosaic.org">www.mosaic.org</a></p>
-	<p>The Fox Theater<br />301 South Garey Ave., Pomona</p>
-	<p>Sundays | 10am</p>
-	</div>
-	<h2>Whittier</h2>
-	<div class="church"><h3>Mosaic Whittier</h3>
-	<p>626-628-9640<br />
-		<a target="_blank" href="http://www.mosaic.org">www.mosaic.org</a></p>
-	<p>7320 Greenleaf Ave., Whittier</p>
-	<p>Sundays | 10am</p>
-	</div>
-
+				<h2>Los Angeles County</h2>
+				<div class="region">
+					<a class="city" href="#longbeach" id="longbeach">Long Beach</a>
+					<div class="churches">
+						<div class="church">
+							<h3>The Garden</h3>
+							<p>
+								<a target="_blank" href="http://www.thegardenlb.org">www.thegardenlb.org</a><br />
+								1001 E. 3rd St., Long Beach<br />
+								Sundays | 10am
+							</p>
+						</div>
+						<div class="church">
+							<h3>Grace Brethren</h3>
+							<p>
+								562-595-6881<br />
+								<a target="_blank" href="http://www.gracelb.net">www.gracelb.net</a><br />
+								3601 Linden Ave, Long Beach<br />
+								Sundays | 9:30am &amp; 11am
+							</p>
+						</div>
+					</div>
+					<a class="city" href="#losangeles" id="losangeles">Los Angeles</a>
+					<div class="churches">
+						<div class="church">
+							<h3>Basileia</h3>
+							<p>
+								323-793-5732<br />
+								<a target="_blank" href="http://www.basileiacommunity.org">www.basileiacommunity.org</a><br />
+								4903 Fountain Ave., Hollywood<br />
+								Sundays | 10:30am
+							</p>
+						</div>
+						<div class="church">
+							<h3>Ecclesia</h3>
+							<p>
+								323-464-8455<br />
+								<a target="_blank" href="http://www.churchinhollywood.com">www.churchinhollywood.com</a><br />
+								The Hollywood Pacific Theatre<br />
+								6433 Hollywood Blvd., Hollywood<br />
+								Sundays | 9:30 &amp; 11:15am
+							</p>
+						</div>
+						<div class="church">
+							<h3>Mosaic Hollywood</h3>
+							<p>
+								626-628-9640<br />
+								<a target="_blank" href="http://www.mosaic.org">www.mosaic.org</a><br />
+								7107 Hollywood Blvd, Hollywood<br />
+								Sundays | 10am, 11:30am, 6pm, 8pm
+							</p>
+						</div>
+						<div class="church">
+							<h3>New Song L.A.</h3>
+							<p>
+								310-842-4215<br />
+								<a target="_blank" href="http://la.newsong.net">la.newsong.net</a><br />
+								11177 Overland Ave., Culver City<br />
+								Sundays | 9:30am &amp; 11:30am
+							</p>
+						</div>
+						<div class="church">
+							<h3>Reality L.A.</h3>
+							<p>
+								323-883-1812<br />
+								<a target="_blank" href="http://www.realityla.com">www.realityla.com</a><br />
+								Helen Bernstein High School<br />
+								1309 N. Wilton Pl., Hollywood<br />
+								Sundays | 9am, 12pm, 5pm
+							</p>
+						</div>
+					</div>
+					<a class="city" href="#pasadena" id="pasadena">Pasadena</a>
+					<div class="churches">
+						<div class="church">
+							<h3>Fellowship Monrovia</h3>
+							<p>
+								626-239-8109<br />
+								<a target="_blank" href="http://www.madeforfellowship.com">www.madeforfellowship.com</a><br />
+								Monrovia High School<br />
+								845 W. Colorado Blvd., Monrovia<br />
+								Sundays | 9am &amp; 11am
+							</p>
+						</div>
+					</div>
+					<a class="city" href="#pomona" id="pomona">Pomona</a>
+					<div class="churches">
+						<div class="church">
+							<h3>Mosaic Pomona</h3>
+							<p>
+								626-628-9640<br />
+								<a target="_blank" href="http://www.mosaic.org">www.mosaic.org</a><br />
+								The Fox Theater<br />
+								301 South Garey Ave., Pomona<br />
+								Sundays | 10am
+							</p>
+						</div>
+					</div>
+					<a class="city" href="#whittier" id="whittier">Whittier</a>
+					<div class="churches">
+						<div class="church">
+							<h3>Mosaic Whittier</h3>
+							<p>
+								626-628-9640<br />
+								<a target="_blank" href="http://www.mosaic.org">www.mosaic.org</a><br />
+								7320 Greenleaf Ave., Whittier<br />
+								Sundays | 10am
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
 	</article>
 </section>
 
