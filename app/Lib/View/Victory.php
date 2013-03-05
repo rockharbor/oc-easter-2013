a926f14875cf4b02225cb12311785ad1af6235ef
Jeremy Harris <jharris@rockharbor.org>
2013-03-05 11:25:14 -0800

    Added social links to view image page

--- app/Lib/View/Victory.php
+++ app/Lib/View/Victory.php
@@ -8,7 +8,14 @@
 			<img src="/uploads/<?php echo $result->filename; ?>" alt="Victory" />
 		</div>
 		<p>
-			<?php echo $result->note; ?>
+			<?php echo $result->note; ?><br />
+			<?php
+			$message = rawurlencode($result->note.' #heavencrashesin ');
+			$page = rawurlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+			$pagetitle = rawurlencode('OC Easter 2013');
+			?>
+			<a href="http://twitter.com/home?status=<?php echo $message.$page; ?>" target="_blank" class="twitter">Twitter</a>
+			<a href="http://www.facebook.com/sharer.php?u=<?php echo $page; ?>&t=<?php echo $pagetitle; ?>" target="_blank" class="facebook">Facebook</a>
 		</p>
 		<?php endif; ?>
 	</article>
