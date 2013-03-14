d9a7eb21114db8028822570c8fdafe219de6ae50
Jeremy Harris <jharris@rockharbor.org>
2013-03-14 16:18:16 -0700

    Updated what is heaven crashing in content

--- app/Lib/View/WhatIsHeavenCrashingIn.php
+++ app/Lib/View/WhatIsHeavenCrashingIn.php
@@ -1,9 +1,15 @@
 <section>
 	<article>
 		<h1>What is <span class="hic">Heaven Crashing In</span>?</h1>
-		<p><span class="hic">Heaven Crashing In</span> is a rebellion to see heaven crash into earth.</p>
+		<video
+			controls
+			style="width: 99%"
+			src="http://dj543q8jb3awo.cloudfront.net/easter2013-promo.mp4"
+			preload="none"
+			data-streamfile="rtmp://s3jkg7i9imyu37.cloudfront.net/cfx/st/mp4:easter2013-promo.mp4"
+		></video>
 		<p>
-			<strong>There is a battle going on around us.</strong><br />
+			There is a battle going on around us.<br />
 			It's entered our neighborhoods and our homes.<br />
 			It's transcended economic and religious and political lines.<br />
 			It's caught all of us in its wake.
@@ -15,13 +21,13 @@
 		</p>
 		<p>Call it what you will, the world we live in is broken.</p>
 		<p>
-			<strong>But we are not forsaken.</strong><br />
+			But we are not forsaken.<br />
 			Centuries ago, Jesus &mdash; the Son of God and the very Author of life &mdash; came to reclaim the world. He died on a cross and rose again three days later to show that death can be defeated. It <em>will</em> be defeated.
 		</p>
 		<p>His victory has assured complete victory.</p>
 		<p>That day is coming. It's not by wishful thinking or aimless aspiration. It's by a thunderous declaration from the One that created everything.</p>
 		<p>
-			<strong>We have reason to hope.</strong><br />
+			We have reason to hope.<br />
 			While we live in-between the first and the final victory, we also live within reach of our own victory...because this is not just something that's happened. Nor is it just something that will happen.
 		</p>
 		<p>Victory is <em>happening</em>.</p>
@@ -39,13 +45,6 @@
 			Here's to the One who's made it possible.
 		</p>
 		<p>Here's... to HEAVEN CRASHING IN.</p>
-		<video
-			controls
-			style="width: 100%"
-			src="http://dj543q8jb3awo.cloudfront.net/easter2013-promo.mp4"
-			preload="none"
-			data-streamfile="rtmp://s3jkg7i9imyu37.cloudfront.net/cfx/st/mp4:easter2013-promo.mp4"
-		></video>
 	</article>
 </section>
 
