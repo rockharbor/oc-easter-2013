46527efa9003b48d4f0a940abafa82fa0d108f1d
Jeremy Harris <jharris@rockharbor.org>
2013-03-25 07:40:41 -0700

    Added stories

--- app/Lib/View/Index.php
+++ app/Lib/View/Index.php
@@ -11,14 +11,31 @@
 		></video>
 	</article>
 
+	<article id="story16">
+		<p>A little over 4 years ago I had to go to the emergency room for an infected cyst on my tailbone that left me in a pain I can&apos;t even describe. I could barely walk, sit, stand or lay down. I was out of work for a week and it took about 3 weeks to heal &emdash; they told me if it came back again I would have to have surgery.</p>
+		<p>For the last 4 years, I have consistently had minor pains because it never fully healed correctly. I also had a horrible scar from the procedure. Over time, it got pretty bad. I was confident I would need surgery.</p>
+		<p>One Sunday night everything changed while I was at church. When they asked if anyone wanted prayer I just couldn&apos;t bring myself to go to the front to be prayed for. God has a sense of humor because someone came up to me asked me if I needed prayer and I still said no!</p>
+		<p>At that point, I didn&apos;t really believe that God could heal me, but  I prayed anyway. The next morning I woke up and 95% of my pain was gone! And by the next day ALL of my pain was gone. If that wasn&apos;t enough, my scar from 4 years ago is visibly smaller! I wasn&apos;t just healed but I was made better then I was before!</p>
+	</article>
+
+	<article id="story17">
+		<p>I am a philosophy student. It&apos;s my job to question everything and look for the ways I can poke holes in every system. My friend brought me to a church thing called &quote;Alpha&quote; which is where you are supposed to be able to ask questions about Christianity and faith and things like that. As an atheist, this seemed like the perfect opportunity to poke some holes in Christianity. When I got to Alpha, I was welcomed to ask questions and question the things I didn&apos;t understand. They actually WANTED me to challenge them! Over the next few months, as much as I tried, something was happening. People answered my questions and stood up to my arguments, but they were not forceful or judgmental. They were kind and I actually felt really loved. We went to the mountains for a weekend and this is when everything changed. For the first time, I started to believe that God was real and that he cared about me. I still have questions, but I know deep down that God is real and that I need to be open to seeing how He working in the world &emdash; and in my own life.</p>
+	</article>
+
+	<article id="story18">
+		<p>My motto for life had been simple: have fun now, get serious later. As a college student, I was focused on doing well in school, but most importantly, having a good time (you know what that means) with my friends. I knew it probably wasn&apos;t the best idea – especially after the time I woke up from the most wild night I have ever been apart of and I didn&apos;t remember anything that happened. I was so ashamed because I knew that things got really out of control.</p>
+		<p>After this night, I started feeling really bad about myself, but I didn&apos;t know what to do. I had been thinking that having fun was the most important thing, but living like this wasn&apos;t fun anymore. It was actually really scary. None of my friends understood how I felt because they were still concerned with having fun all the time. I remembered a friend of mine that said she went to a church that was really cool. I don&apos;t know what made me think of her and this church, but I figured there was nothing better I could do. I called her and asked if she wanted to meet up for dinner. I was kind of scared to meet up with her because I thought for sure she would tell me I was hopeless and that even church or God couldn&apos;t help me.</p>
+		<p>That night at dinner, she told me that there is a God who loved me and who was much more fulfilling than any of the stuff I was doing. She told me that she was just like me, obsessed with partying and drinking, but then she found Jesus and is more happy than she&apos;s ever been. Something about what she was saying was so intriguing to me and I agreed to go to church with her. Flash forward two months, and I have become a Christian! I know that God gives me more joy than anything else. I still have make mistakes, but I know that I am not alone and that I have God to listen and guide me.</p>
+	</article>
+
 	<article id="story13">
-		<p>For most of my life, I hated the church and Christians and everything that had to do with any religious thing. I always felt ridiculed and judged, and never once had an encounter with a "Jesus follower" that was positive. Two years ago, my in-laws dragged me [kicking and screaming] to an Easter church service.</p>
-		<p>I can't describe what happened to me that day but what I do know is that I am not the same person that walked into that service. All the bitterness and anger toward the Church and Christians suddenly disappeared. Now I am excited when I meet someone who is angry or bitter toward the Church, because I can share my experience with them and tell them about the God who showed me His deep love, even when I was not looking for it.</p>
+		<p>For most of my life, I hated the church and Christians and everything that had to do with any religious thing. I always felt ridiculed and judged, and never once had an encounter with a &quote;Jesus follower&quote; that was positive. Two years ago, my in-laws dragged me [kicking and screaming] to an Easter church service.</p>
+		<p>I can&apos;t describe what happened to me that day but what I do know is that I am not the same person that walked into that service. All the bitterness and anger toward the Church and Christians suddenly disappeared. Now I am excited when I meet someone who is angry or bitter toward the Church, because I can share my experience with them and tell them about the God who showed me His deep love, even when I was not looking for it.</p>
 	</article>
 
 	<article id="story14">
 		<p>A few weeks ago in church the pastor told us to pray for the very last person on Earth that we thought would become a Christian. Instantly my brother came to mind. </p>
-		<p>I wrote his name down and committed to praying for him daily. If I am honest, I didn't fully expect something to happen...at least not right away. Little did I know that God was already at work in my brother's life. Within a few weeks of me starting to pray for him, I got a call from my brother saying that He made a decision to follow Jesus.</p>
+		<p>I wrote his name down and committed to praying for him daily. If I am honest, I didn&apos;t fully expect something to happen...at least not right away. Little did I know that God was already at work in my brother&apos;s life. Within a few weeks of me starting to pray for him, I got a call from my brother saying that He made a decision to follow Jesus.</p>
 	</article>
 
 	<article id="story15">
@@ -29,7 +46,7 @@
 	<article id="story1">
 		<p>On January 6th, I gathered a group of girls on the Prayer Team to pray for a 14-year old who has just been diagnosed with cancer. She was really scared and could barely tell us what was wrong and how we could pray for her. We prayed and asked God to heal her.</p>
 		<p>She went in for some follow up tests and was told that the cancer had miraculously disappeared. The doctors were baffled, but we all knew that she had been healed because of prayer.</p>
-		<p>On February 24th, I got the chance to baptize this girl as she boldly declared that she wants to live a life dedicated to sharing her story of VICTORY and God's radical work in her life.</p>
+		<p>On February 24th, I got the chance to baptize this girl as she boldly declared that she wants to live a life dedicated to sharing her story of VICTORY and God&apos;s radical work in her life.</p>
 	</article>
 
 	<article id="story2">
@@ -55,7 +72,7 @@
 	</article>
 
 	<article id="story4">
-		<p>There are 163 million orphans in the world. From a very young age, I knew I wanted to do something about this problem. I knew I couldn't solve it entirely, but there had to be something I could do...</p>
+		<p>There are 163 million orphans in the world. From a very young age, I knew I wanted to do something about this problem. I knew I couldn&apos;t solve it entirely, but there had to be something I could do...</p>
 		<p>My husband and I have adopted two children&mdash;one from the American foster care system, and another from Haiti. We claim victory for them as we raise them to love God and to be thankful for all the blessings they have.</p>
 	</article>
 
@@ -83,7 +100,7 @@
 
 	<article id="story7">
 		<p>My mom has been a drug addict and alcoholic since I was born. When my siblings and I were teenagers, she had an affair, and we watched her suffer emotional and mental abuse from my dad because she was unfaithful to him. They ended up getting divorced, and we were left fatherless, and really, motherless as well. I watched her life spiral out of control, often.</p>
-		<p>Over the last few years, she started going to a Life Group, and eventually to church on Sundays. It's been crazy to see her life turn around, and change for the better. She is picking up the pieces, one by one, and is letting God have control in her life. She is finally experiencing victory, and even though it's a hard road, I know that something in her has changed for good.</p>
+		<p>Over the last few years, she started going to a Life Group, and eventually to church on Sundays. It&apos;s been crazy to see her life turn around, and change for the better. She is picking up the pieces, one by one, and is letting God have control in her life. She is finally experiencing victory, and even though it&apos;s a hard road, I know that something in her has changed for good.</p>
 	</article>
 
 	<article id="story8">
@@ -109,18 +126,18 @@
 	</article>
 
 	<article id="story10">
-		<p>I woke up after a night of drinking in Vegas and my hand was in excruciating pain – bruised black and blue. I had absolutely no recollection of what had happened or how it ended up that way. After going to the doctor for my hand, I was diagnosed with an auto-immune disease.</p>
-		<p>Months later, a friend and I googled churches and found ourselves sitting in the seats the next Sunday. For some reason, that was the only place I wanted to be. I ended up asking for prayer for my hand. A few days later, my hand was healed and there was no trace of that disease. I still can't explain it. It's a miracle.</p>
+		<p>I woke up after a night of drinking in Vegas and my hand was in excruciating pain &emdash; bruised black and blue. I had absolutely no recollection of what had happened or how it ended up that way. After going to the doctor for my hand, I was diagnosed with an auto-immune disease.</p>
+		<p>Months later, a friend and I googled churches and found ourselves sitting in the seats the next Sunday. For some reason, that was the only place I wanted to be. I ended up asking for prayer for my hand. A few days later, my hand was healed and there was no trace of that disease. I still can&apos;t explain it. It&apos;s a miracle.</p>
 	</article>
 
 	<article id="story11">
-		<p>I dealt drugs in high school. Even though I made money doing it, I couldn't find a way for it to make me happy.</p>
-		<p>One day I watched this video of an ex-addict and famous musician talk about being happy because he found Jesus. It's hard to imagine, but my life has been different ever since; I've given up drugs and alcohol, and now dedicate my time to working with high school students. I tell them the simple truth that using can never make them happy.</p>
+		<p>I dealt drugs in high school. Even though I made money doing it, I couldn&apos;t find a way for it to make me happy.</p>
+		<p>One day I watched this video of an ex-addict and famous musician talk about being happy because he found Jesus. It&apos;s hard to imagine, but my life has been different ever since; I&apos;ve given up drugs and alcohol, and now dedicate my time to working with high school students. I tell them the simple truth that using can never make them happy.</p>
 	</article>
 
 	<article id="story12">
 		<p>I grew up without a father. When I was a baby, mine committed suicide – and so I never got to meet him. Every time someone talked about their dad or mentioned how awesome their family life was...I felt a sharp pain stabbing at my heart. I would never know of that kind of life.</p>
-		<p>Now many years later, I still don't have a father. But what I do have is people around me – friends and family – that love me and care for me. It doesn't replace a father's love...but it does make it easier. Because of these great people in my life, I now work with foster kids and orphans who might feel the same way I used to. I want them to know that they are loved!</p>
+		<p>Now many years later, I still don&apos;t have a father. But what I do have is people around me – friends and family – that love me and care for me. It doesn&apos;t replace a father&apos;s love...but it does make it easier. Because of these great people in my life, I now work with foster kids and orphans who might feel the same way I used to. I want them to know that they are loved!</p>
 	</article>
 
 </section>
@@ -142,6 +159,9 @@ $pagetitle = rawurlencode('OC Easter 2013');
 
 <div class="multi background">
 <div
+	class="bg26"></div><div
+	class="bg27"></div><div
+	class="bg28"></div><div
 	class="bg10"></div><div
 	class="bg23"></div><div
 	class="bg24"></div><div
